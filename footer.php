33cab6dbc8839ea0586b8afc21aa6a7e8dbdca94
Benjamin Lu <benlumia007@users.noreply.github.com>
2020-11-14 16:56:52 -0800

    Version: 0.1.8

--- footer.php
+++ footer.php
@@ -1,5 +1,6 @@
     </section>
-    <footer class = "site-footer">        
+    <footer class = "site-footer">  
+        <?php _e('Theme By: Benjamin Lu <br />'); ?> <a href = "http://wordpress.org"><?php _e('Powered By: WordPress'); ?></a>
     </footer>
 <?php wp_footer(); ?>
 </body>        
