3f790ebfc867e81ba9342a08991e811d1c946d2f
Benjamin Lu <benlumia007@users.noreply.github.com>
2020-12-19 22:04:30 -0800

    moved the menu on top

--- header.php
+++ header.php
@@ -1,11 +1,11 @@
 <?php
 /**
- * Initiator ( header.php )
+ * Silver Quantum ( header.php )
  *
- * @package     Initiator
- * @copyright   Copyright (C) 2019. Benjamin Lu
- * @license     GNU General Public License v2 or later ( https://www.gnu.org/licenses/gpl-2.0.html )
- * @author      Benjamin Lu ( https://benjlu.com )
+ * @package   Silver Quantum
+ * @copyright Copyright (C) 2014-2020. Benjamin Lu
+ * @license   GNU General Public License v2 or later ( https://www.gnu.org/licenses/gpl-2.0.html )
+ * @author    Benjamin Lu ( https://benjlu.com )
  */
 ?>
 <!doctype html>
@@ -18,12 +18,14 @@
 </head>
 <body <?php body_class(); ?>>
 <?php wp_body_open(); ?>
+<a class="skip-link screen-reader-text" href="#main"><?php esc_html_e( 'Skip to content', 'initiator' ) ?></a>
+<div id="header" class="main-navigation">
+	<?php Benlumia007\Backdrop\View\display( 'menu', [ 'primary' ] ); ?>
+</div>
 <div id="container" class="site-container">
-	<a class="skip-link screen-reader-text" href="#main"><?php esc_html_e( 'Skip to content', 'initiator' ) ?></a>
-	<header id="header" class="site-header">
+	<header id="site-header" class="site-header header-image">
 		<div class="site-branding">
 			<?php Benlumia007\Backdrop\Site\display_site_title(); ?>
 			<?php Benlumia007\Backdrop\Site\display_site_description(); ?>
 		</div>
-		<?php Benlumia007\Backdrop\View\display( 'menu', [ 'primary' ] ); ?>
 	</header>
