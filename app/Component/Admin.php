6df9d6a9e6daa674ff2f9fc980875b3bee8070e5
Benjamin Lu <benlumia007@users.noreply.github.com>
2020-12-20 16:47:26 -0800

    readjsut

--- app/Component/Admin.php
+++ app/Component/Admin.php
@@ -1,14 +1,14 @@
 <?php // phpcs:ignore
 /**
- * Backdrop Core ( Admin.php )
+ * Silver Quantum ( Admin.php )
  *
- * @package     Backdrop Core
- * @copyright   Copyright (C) 2019. Benjamin Lu
- * @license     GNU General PUblic License v2 or later ( https://www.gnu.org/licenses/gpl-2.0.html )
- * @author      Benjamin Lu ( https://benjlu.com )
+ * @package   Silver Quantum
+ * @copyright Copyright (C) 2014-2020. Benjamin Lu
+ * @license   GNU General PUblic License v2 or later ( https://www.gnu.org/licenses/gpl-2.0.html )
+ * @author    Benjamin Lu ( https://benjlu.com )
  */
 
-namespace Initiator\Component;
+namespace SilverQuantum\Component;
 
 use Benlumia007\Backdrop\Contracts\Admin\Admin as AdminPage;
 
