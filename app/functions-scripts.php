496fb68f3fac5e95dc5f4c01acd2ef02a26cf4a0
Benjamin Lu <benlumia007@users.noreply.github.com>
2020-12-20 16:20:19 -0800

    rename text domain

--- app/functions-scripts.php
+++ app/functions-scripts.php
@@ -1,17 +1,17 @@
 <?php
 /**
- * Initiator ( functions-scripts.php )
+ * Silver Quantum ( functions-scripts.php )
  *
- * @package     Initiator
- * @copyright   Copyright (C) 2019. Benjamin Lu
- * @license     GNU General Public License v2 or later ( https://www.gnu.org/licenses/gpl-2.0.html )
- * @author      Benjamin Lu ( https://benjlu.com )
+ * @package   Silver Quantum
+ * @copyright Copyright (C) 2014-2020. Benjamin Lu
+ * @license   GNU General Public License v2 or later ( https://www.gnu.org/licenses/gpl-2.0.html )
+ * @author    Benjamin Lu ( https://benjlu.com )
  */
 
 /**
  * Define namespace
  */
-namespace Initiator;
+namespace SilverQuantum;
 
 /**
  * Enqueue Scripts and Styles
@@ -30,12 +30,12 @@ add_action(
 		 * Rather than enqueue the main stylesheet, we are going to enqueue sceen.css since all of the styles will
 		 * go here. We only need parse the information for the Theme in style.css so that it can be activated.
 		 */
-		wp_enqueue_style( 'initiator-screen', get_theme_file_uri( 'public/css/screen.css' ), array(), '1.0.0' );
+		wp_enqueue_style( 'silver-quantum-screen', get_theme_file_uri( 'public/css/screen.css' ), array(), '1.0.0' );
 
 		/**
 		 * We will be enqueue the app.js file, which mainly be for the navigation only.
 		 */
-		wp_enqueue_script( 'initiator-app', get_theme_file_uri( 'public/js/app.js' ), array('jquery'), '1.0.0', true );
+		wp_enqueue_script( 'silver-quantum-app', get_theme_file_uri( 'public/js/app.js' ), array('jquery'), '1.0.0', true );
 
 		/**
 		 * This allows users to comment by clicking on reply so that it gets nested.
@@ -60,13 +60,40 @@ add_action(
 				{$value}
 			}
 		";
-		wp_add_inline_style( 'initiator-style', $custom_css );
+		wp_add_inline_style( 'silver-quantum-style', $custom_css );
+	}
+);
+
+add_action(
+	'wp_enqueue_scripts', function() {
+		$custom_image = esc_url( get_theme_mod( 'header_image', get_theme_file_uri( '/public/images/header-image.jpg' ) ) );
+	
+		$custom_css = "      
+			.site-header.header-image {
+				background: url({$custom_image});
+				background-attachment: fixed;
+				background-position: center;
+			}
+			
+			@media screen and (max-width: 30em) {
+				.site-header {
+					padding-top: 10em;
+				}
+			}
+
+			@media screen and ( min-width: 30.063em ) and ( max-width: 37.5em ) {
+				.site-header {
+					padding-top: 15em;
+				}
+			}
+		";
+		wp_add_inline_style( 'silver-quantum-screen', $custom_css );
 	}
 );
 
 add_action(
 	'enqueue_block_editor_assets',
 	function() {
-		wp_enqueue_style( 'initiator-custom-fonts', get_theme_file_uri( '/vendor/benlumia007/backdrop-core/assets/fonts/custom-fonts.css' ), array(), '1.0.0' );
+		wp_enqueue_style( 'silver-quantum-custom-fonts', get_theme_file_uri( '/vendor/benlumia007/backdrop-core/assets/fonts/custom-fonts.css' ), array(), '1.0.0' );
 	}
 );
