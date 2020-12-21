75c6a76ee0adb4fd6ad8e81421c4a8cf88d586ee
Benjamin Lu <benlumia007@users.noreply.github.com>
2020-12-20 16:19:25 -0800

    rename text domain and add social navigation and add additional sidebars

--- app/framework.php
+++ app/framework.php
@@ -1,13 +1,13 @@
 <?php
 /**
- * Initiator ( framework.php )
+ * Silver Quantum ( framework.php )
  *
  * This file is used to create a new framework instance and adds specific features to the theme.
  *
- * @package     Initiator
- * @copyright   Copyright (C) 2019. Benjamin Lu
- * @license     GNU General Public License v2 or later ( https://www.gnu.org/licenses/gpl-2.0.html )
- * @author      Benjamin Lu ( https://benjlu.com )
+ * @package   Silver Quantum
+ * @copyright Copyright (C) 2014-2020. Benjamin Lu
+ * @license   GNU General Public License v2 or later ( https://www.gnu.org/licenses/gpl-2.0.html )
+ * @author    Benjamin Lu ( https://benjlu.com )
  */
 
 /**
@@ -15,19 +15,24 @@
  *
  * This will create an instance of the framework allowing you to initialize the theme.
  */
-$initiator = Benlumia007\Backdrop\Framework::get_instance();
+$silver_quantum = Benlumia007\Backdrop\Framework::get_instance();
 
-$initiator->menus = new Benlumia007\Backdrop\Menu\Menu(
+$silver_quantum->menus = new Benlumia007\Backdrop\Menu\Menu(
 	$args = [
-		'primary' => esc_html__( 'Primary Navigation', 'initiator' ),
+		'primary' => esc_html__( 'Primary Navigation', 'silver-quantum' ),
+		'socal' => esc_html__( 'Social Navigation', 'silver-quantum' ),
 	]
 );
 
-$initiator->sidebars = new Benlumia007\Backdrop\Sidebar\Sidebar(
+$silver_quantum->sidebars = new Benlumia007\Backdrop\Sidebar\Sidebar(
 	$args = [
 		'primary' => [
-			'name' => esc_html__( 'Primary Sidebar', 'initiator' ),
-			'desc' => esc_html__( 'Test', 'initiator' ),
+			'name' => esc_html__( 'Primary Sidebar', 'silver-quantum' ),
+			'desc' => esc_html__( 'Primary', 'silver-quantum' ),
 		],
+		'secondary' => [
+			'name' => esc_html__( 'Secondary Sidebar', 'silver-quantum' ),
+			'desc' => esc_html__( 'Pages', 'silver-quantum' ),
+		]
 	]
 );
