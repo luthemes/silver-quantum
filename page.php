0cf58c57c76adfc427928148fc0adfc0e486f8b1
Benjamin Lu <benlumia007@users.noreply.github.com>
2021-03-31 08:01:31 -0700

    minor changes

--- page.php
+++ page.php
@@ -3,14 +3,14 @@
  * Silver Quantum ( page.php )
  *
  * @package   Silver Quantum
- * @copyright Copyright (C) 2014-2020. Benjamin Lu
+ * @copyright Copyright (C) 2014-2021. Benjamin Lu
  * @license   GNU General Public License v2 or later ( https://www.gnu.org/licenses/gpl-2.0.html )
- * @author    Benjamin Lu ( https://benjlu.com )
+ * @author    Benjamin Lu ( https://getbenonit.com )
  */
 ?>
 <?php get_header(); ?>
 	<section id="content" class="site-content">
-		<div id="global-layout" class="<?php echo esc_attr( get_theme_mod( 'global_layout', 'no-sidebar' ) ); ?>">
+		<div id="global-layout" class="<?php echo esc_attr( get_theme_mod( 'global_layout', 'left-sidebar' ) ); ?>">
 			<main id="main" class="content-area">
 				<?php
 					while ( have_posts() ) : the_post();
