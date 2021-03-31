f237c1e589ce6e5523b7f7fc5cdb525af87cce5d
Benjamin Lu <benlumia007@users.noreply.github.com>
2021-03-31 08:11:31 -0700

    change no-sidebar to left-sidebar

--- search.php
+++ search.php
@@ -2,15 +2,15 @@
 /**
  * Silver Quantum ( search.php )
  *
- * @package     Silver Quantum
- * @copyright   Copyright (C) 2019. Benjamin Lu
- * @license     GNU General Public License v2 or later ( https://www.gnu.org/licenses/gpl-2.0.html )
- * @author      Benjamin Lu ( https://benjlu.com )
+ * @package   Silver Quantum
+ * @copyright Copyright (C) 2014-2021. Benjamin Lu
+ * @license   GNU General Public License v2 or later ( https://www.gnu.org/licenses/gpl-2.0.html )
+ * @author    Benjamin Lu ( https://getbenonit.com )
  */
 ?>
 <?php get_header(); ?>
 	<section id="content" class="site-content">
-		<div id="layout" class="<?php echo esc_attr( get_theme_mod( 'global_layout', 'no-sidebar' ) ); ?>">
+		<div id="layout" class="<?php echo esc_attr( get_theme_mod( 'global_layout', 'left-sidebar' ) ); ?>">
 			<main id="main" class="content-area">
 				<?php
 					if ( have_posts() ) :
