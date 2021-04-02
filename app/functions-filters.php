942985d47843368406ba18e922855c582e3e1e92
Benjamin Lu <benlumia007@users.noreply.github.com>
2021-04-01 17:21:38 -0700

    updated

--- app/functions-filters.php
+++ app/functions-filters.php
@@ -50,7 +50,7 @@ add_filter( 'get_the_archive_title', function() {
 	} elseif ( is_year() ) {
 		$title = esc_html__( 'Year', 'silver-quantum' ) . '<span class="archive-description">' . get_the_date( _x( 'Y', 'yearly archives date format', 'silver-quantum' ) ) . '</span>';
 	} elseif ( is_month() ) {
-		$title = esc_html__( 'Month', 'silver-quantum' ) . '<span class="archive-description">' . get_the_date( _x( 'F', 'monthly archives date format', 'silver-quantum' ) ) . '</span>';
+		$title = esc_html__( 'Month', 'silver-quantum' ) . '<span class="archive-description">' . get_the_date( _x( 'F Y', 'monthly archives date format', 'silver-quantum' ) ) . '</span>';
 	} elseif ( is_day() ) {
 		$title = esc_html__( 'Day', 'silver-quantum' ) . '<span class="archive-description">' . get_the_date( _x( 'F j, Y', 'daily archives date format', 'silver-quantum' ) ) . '</span>';
 	} elseif ( is_tax( 'post_format' ) ) {
