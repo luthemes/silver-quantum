0f7498cc7a041f41cb6b8e4ee1f4331a08019934
Benjamin Lu <benlumia007@users.noreply.github.com>
2021-10-15 10:58:32 -0700

    this file should not be used due to the new template

--- index.php
+++ index.php
@@ -1,29 +1,15 @@
 <?php
 /**
- * Silver Quantum ( index.php )
+ * Index template.
  *
- * @package   Silver Quantum
- * @copyright Copyright (C) 2014-2021. Benjamin Lu
- * @license   GNU General Public License v2 or later ( https://www.gnu.org/licenses/gpl-2.0.html )
- * @author    Benjamin Lu ( https://benjlu.com )
+ * This template should never be loaded on a site unless a plugin is doing
+ * something that it shouldn't be doing or something weird is going on. We're
+ * leaving the below code as an absolute fallback in case this file is loaded.
+ * All it does is correctly load up our `public/views/index.php` template.
  */
-?>
-<?php get_header(); ?>
-	<section id="content" class="site-content">
-		<div id="layout" class="<?php echo esc_attr( get_theme_mod( 'global_layout', 'left-sidebar' ) ); ?>">
-			<main id="main" class="content-area">
-				<?php
-					if ( have_posts() ) :
-						while ( have_posts() ) : the_post();
-							Benlumia007\Backdrop\Template\get_template_part( 'content/content', get_post_format() );
-						endwhile;
-						the_posts_pagination();
-					else :
-							Benlumia007\Backdrop\Template\get_template_part( 'content/content', 'none' );
-					endif;
-				?>
-			</main>
-			<?php Benlumia007\Backdrop\View\display( 'sidebar', [ 'primary' ] ); ?>
-		</div>
-	</section>
-<?php get_footer(); ?>
+
+// Access the view template engine.
+$engine = Benlumia007\Backdrop\App::resolve( 'view/engine' );
+
+// Load the index template.
+$engine->display( 'index' );
