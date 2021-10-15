73fc8172a4781cc2fae7fda02985646967eb78bb
Benjamin Lu <benlumia007@users.noreply.github.com>
2021-10-15 11:03:47 -0700

    initial commit

---
+++ public/views/header.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Default Header
+ *
+ * @package   Silver Quantum
+ * @author    Benjamin Lu ( benlumia007@gmail.com )
+ * @copyright Copyright (C) 2014-2021. Benjamin Lu
+ * @license   https://www.gnu.org/licenses/gpl-2.0.html
+ * @link      https://luthemes.com/portfolio/silver-quantum
+ */
+?>
+<!doctype html>
+<html <?php language_attributes(); ?>>
+<head>
+<meta charset="<?php bloginfo( 'charset' ); ?>" />
+<meta name="viewport" content="width=device-width, initial-scale=1" />
+<link rel="profile" href="https://gmpg.org/xfn/11" />
+<?php wp_head(); ?>
+</head>
+<body <?php body_class(); ?>>
+<?php wp_body_open(); ?>
+<a class="skip-link screen-reader-text" href="#main"><?php esc_html_e( 'Skip to content', 'silver-quantum' ) ?></a>
+<div id="masthead" class="top-navigation">
+	<?php Benlumia007\Backdrop\Theme\Menu\display( 'menu', [ 'primary' ] ); ?>
+</div>
+<div id="container" class="site-container">
+	<header id="site-header" class="site-header header-image">
+		<div class="site-branding">
+			<?php Benlumia007\Backdrop\Theme\Site\display_site_title(); ?>
+			<?php Benlumia007\Backdrop\Theme\Site\display_site_description(); ?>
+		</div>
+	</header>
---
+++ public/views/index.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Main Template File - index.php
+ *
+ * @package   Silver Quantum
+ * @author    Benjamin Lu ( benlumia007@gmail.com )
+ * @copyright Copyright (C) 2014-2021. Benjamin Lu
+ * @license   https://www.gnu.org/licenses/gpl-2.0.html
+ * @link      https://luthemes.com/portfolio/silver-quantum
+ */
+?>
+<?php $engine = Benlumia007\Backdrop\App::resolve( 'view/engine' ); ?>
+<?php $engine->display( 'header' ); ?>
+<?php $engine->display( 'footer' ); ?>
