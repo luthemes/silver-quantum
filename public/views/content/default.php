47a8df49f24d3cc5e704a9371042c027b5275d49
Benjamin Lu <benlumia007@users.noreply.github.com>
2021-10-15 11:10:34 -0700

    add content for the home/blog

--- public/views/content/default.php
+++ public/views/content/default.php
@@ -2,10 +2,11 @@
 /**
  * silver-quantum ( content.php )
  *
- * @package     silver-quantum
- * @copyright   Copyright (C) 2018-2019. Benjamin Lu
- * @license     GNU General Public License v2 or later ( https://www.gnu.org/licenses/gpl-2.0.html )
- * @author      Benjamin Lu ( https://benjlu.com )
+ * @package   Silver Quantum
+ * @author    Benjamin Lu ( benlumia007@gmail.com )
+ * @copyright Copyright (C) 2014-2021. Benjamin Lu
+ * @license   https://www.gnu.org/licenses/gpl-2.0.html
+ * @link      https://luthemes.com/portfolio/silver-quantum
  */
 ?>
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
@@ -13,11 +14,11 @@
 		<?php if ( is_sticky() && is_home() && ! is_paged() ) { ?>
 			<?php printf( '<span class="sticky-post">%1$s</span>', esc_html__( 'Featured', 'silver-quantum' ) ); ?>
 		<?php } ?>
-		<?php Benlumia007\Backdrop\Entry\display_title(); ?>
+		<?php Benlumia007\Backdrop\Theme\Entry\display_title(); ?>
 		<div class="entry-metadata">
-			<?php Benlumia007\Backdrop\Entry\display_author(); ?>
-			<?php Benlumia007\Backdrop\Entry\display_date(); ?>
-			<?php Benlumia007\Backdrop\Entry\display_comments_link(); ?>
+			<?php Benlumia007\Backdrop\Theme\Entry\display_author(); ?>
+			<?php Benlumia007\Backdrop\Theme\Entry\display_date(); ?>
+			<?php Benlumia007\Backdrop\Theme\Entry\display_comments_link(); ?>
 		</div>
 	</header>
 	<div class="post-thumbnails">
--- public/views/index.php
+++ public/views/index.php
@@ -11,4 +11,17 @@
 ?>
 <?php $engine = Benlumia007\Backdrop\App::resolve( 'view/engine' ); ?>
 <?php $engine->display( 'header' ); ?>
+    <section id="content" class="site-content">
+        <div id="global-layout" class="<?php echo esc_attr( get_theme_mod( 'global_layout', 'left-sidebar' ) ); ?>">
+            <main id="main" class="content-area">
+                <?php
+                    while ( have_posts() ) : the_post();
+                        $engine->display( 'content' );
+                    endwhile;
+                    comments_template();
+                ?>
+            </main>
+            <?php Benlumia007\Backdrop\Theme\Sidebar\display( 'sidebar', [ 'primary' ] ); ?>
+        </div>
+    </section>
 <?php $engine->display( 'footer' ); ?>
