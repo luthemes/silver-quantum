0adc72dbbc59b9eeaf3b535d2b6bcf7d08f8a076
Benjamin Lu <benlumia007@users.noreply.github.com>
2023-06-20 17:25:29 -0700

    add new files

---
+++ app/Tools/Config.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * Config Class.
+ *
+ * A simple class for grabbing and returning a configuration file from `/config`.
+ *
+ * @package   Exhale
+ * @author    Justin Tadlock <justintadlock@gmail.com>
+ * @copyright 2019 Justin Tadlock
+ * @license   https://www.gnu.org/licenses/gpl-2.0.html GPL-2.0-or-later
+ * @link      https://themehybrid.com/themes/exhale
+ */
+
+namespace SilverQuantum\Tools;
+
+/**
+ * Config class.
+ *
+ * @since  1.0.0
+ * @access public
+ */
+
+class Config {
+
+	/**
+	 * Includes and returns a given PHP config file. The file must return
+	 * an array.
+	 *
+	 * @since  1.0.0
+	 * @access public
+	 * @param  string  $name
+	 * @return array
+	 */
+	public static function get( string $name ) {
+
+		$file = static::path( "{$name}.php" );
+
+		return ( array ) apply_filters( "silver/quantum/config/{$name}/", file_exists( $file ) ? include( $file ) : [] );
+	}
+
+	/**
+	 * Returns the config path or file path if set.
+	 *
+	 * @since  1.0.0
+	 * @access public
+	 * @param  string  $file
+	 * @return string
+	 */
+	public static function path( string $file = '' ) {
+
+		$file = trim( $file, '/' );
+
+		return get_parent_theme_file_path( $file ? "config/{$file}" : 'config' );
+	}
+}
