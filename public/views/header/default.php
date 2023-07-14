d510352328df355bb299e616cd937b40f06cbc5c
Benjamin Lu <benlumia007@users.noreply.github.com>
2023-07-14 11:34:45 -0700

    updated

--- public/views/header/default.php
+++ public/views/header/default.php
@@ -20,7 +20,7 @@
 <body <?php body_class(); ?>>
 <?php wp_body_open(); ?>
 <?php Backdrop\View\display( 'menu', 'primary', [ 'location' => 'primary' ] ); ?>
-<div id="container" class="site-container bg-white mx-18">
+<div id="container" class="site-container xl:container xxl:container">
 	<a class="skip-link screen-reader-text" href="#main"><?php esc_html_e( 'Skip to content', 'silver-quantum' ); ?></a>
 	<header id="masthead" class="site-header">
 		<div class="custom-image-header">
