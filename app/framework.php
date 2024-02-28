5cd9f0b65ee46d69dec189a06a92ddef867e4214
Benjamin Lu <benlumia007@users.noreply.github.com>
2024-02-28 19:36:52 +0000

    updated

--- app/framework.php
+++ app/framework.php
@@ -1,43 +1,67 @@
 <?php
 /**
- * Silver Quantum ( app/framework.php )
+ * Theme framework file.
  *
- * This file is used to create a new framework instance and adds specific features to the theme.
+ * This file is used to create a new application instance and bind items to the
+ * container. This is the heart of the application.
  *
  * @package   Silver Quantum
- * @author    Benjamin Lu ( benlumia007@gmail.com )
- * @copyright Copyright (C) 2014-2021. Benjamin Lu
+ * @author    Benjamin Lu <benlumia007@gmail.com>
+ * @copyright 2014-2024 Benjamin Lu
  * @license   https://www.gnu.org/licenses/gpl-2.0.html
  * @link      https://luthemes.com/portfolio/silver-quantum
  */
 
-/**
- * Create a new framework instance
- *
- * This will create an instance of the framework allowing you to initialize the theme.
- */
-$silver_quantum = Backdrop\booted() ? Backdrop\app() : new Backdrop\Core\Application();
+# ------------------------------------------------------------------------------
+# Create a new application.
+# ------------------------------------------------------------------------------
+#
+# Creates the one true instance of the Backdrop application. You may access this 
+# instance via the `Backdrop\app()` function or `Backdrop\App` static class after 
+# the application has booted.
 
-/**
- * Register Default Providers
- */
-$silver_quantum->provider( Backdrop\Fonts\Provider::class );
-$silver_quantum->provider( Backdrop\Mix\Provider::class );
-$silver_quantum->provider( Backdrop\Template\Hierarchy\Provider::class );
-$silver_quantum->provider( Backdrop\Template\Manager\Provider::class );
-$silver_quantum->provider( Backdrop\Theme\Provider::class );
-$silver_quantum->provider( Backdrop\View\Provider::class );
+$theme = Backdrop\booted() ? Backdrop\app() : new Backdrop\Core\Application();
 
-/**
- * Register Service Provider with the Framework
- */
-// $silver_quantum->provider( SilverQuantum\Admin\Provider::class );
-$silver_quantum->provider( SilverQuantum\Customize\Provider::class );
-$silver_quantum->provider( SilverQuantum\Customize\Layouts\Provider::class );
-// $silver_quantum->provider( SilverQuantum\Menu\Provider::class );
-// $silver_quantum->provider( SilverQuantum\Sidebar\Provider::class );
+# ------------------------------------------------------------------------------
+# Register default service providers with the application.
+# ------------------------------------------------------------------------------
+#
+# Before booting the application, add any service providers that are necessary
+# for running the theme. Service providers are essentially the backbone of the
+# bootstrapping process.
 
-/**
- * Boot the Framework
- */
-$silver_quantum->boot();
+$theme->provider( Backdrop\Fonts\Provider::class );
+$theme->provider( Backdrop\Mix\Provider::class );
+$theme->provider( Backdrop\Template\Hierarchy\Provider::class );
+$theme->provider( Backdrop\Template\Manager\Provider::class );
+$theme->provider( Backdrop\Theme\Provider::class );
+$theme->provider( Backdrop\View\Provider::class );
+
+# ------------------------------------------------------------------------------
+# Register additional service providers with the application.
+# ------------------------------------------------------------------------------
+#
+# Before booting the application, add any additional service providers that are 
+# necessary for running the theme.
+
+// $theme->provider( SilverQuantum\Customize\Provider::class );
+// $theme->provider( SilverQuantum\Customize\Layouts\Provider::class );
+
+# ------------------------------------------------------------------------------
+# Perform bootstrap actions.
+# ------------------------------------------------------------------------------
+#
+# Creates an action hook for child themes (or even plugins) to hook into the
+# bootstrapping process and add their own bindings before the app is booted by
+# passing the application instance to the action callback.
+
+do_action( 'silver/quantum/bootstrap', $theme );
+
+# ------------------------------------------------------------------------------
+# Bootstrap the application.
+# ------------------------------------------------------------------------------
+#
+# Calls the application `boot()` method, which launches the application. Pat
+# yourself on the back for a job well done.
+
+$theme->boot();
