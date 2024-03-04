70402c7ed9123b6036bc03a13f4ecbbaaf88043b
Benjamin Lu <benlumia007@users.noreply.github.com>
2024-03-04 19:56:13 +0000

    updated

--- public/views/entry/single.php
+++ public/views/entry/single.php
@@ -14,6 +14,18 @@
 		</div>
 	</header>
 	<div class="entry-content">
-		<?php the_excerpt(); ?>
+		<?php the_content(); ?>
+		<?php
+			wp_link_pages(
+				array(
+					'before'      => '<div class="page-links">' . esc_html__( 'Pages:', 'silver-quantum' ),
+					'after'       => '</div>',
+					'link_before' => '<span>',
+					'link_after'  => '</span>',
+					'pagelink'    => '<span class="screen-reader-text">' . esc_html__( 'Page', 'silver-quantum' ) . ' </span>%',
+					'separator'   => '<span class="screen-reader-text">,</span> ',
+				)
+			);
+		?>
 	</div>
 </article>
