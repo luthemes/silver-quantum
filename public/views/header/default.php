7b219e905ac9b7285c91a9052838fc3e2f13ab7d
Benjamin Lu <benlumia007@users.noreply.github.com>
2024-03-04 23:05:12 +0000

    add support for custom logo

--- public/views/header/default.php
+++ public/views/header/default.php
@@ -12,11 +12,24 @@
 <div id="container" class="site-container lg:container xl:container bg-white">
 	<a class="skip-link screen-reader-text" href="#main"><?php esc_html_e( 'Skip to content', 'silver-quantum' ); ?></a>
 	<header id="masthead" class="site-header">
-		<div class="custom-image-header">
-			<img alt="" src="<?php header_image(); ?>" width="<?php echo absint( get_custom_header()->width ); ?>" height="<?php echo absint( get_custom_header()->height ); ?>">
+	<div class="custom-image-header">
+		<?php
+			$custom_logo_id = get_theme_mod( 'custom_logo' );
+			$logo = wp_get_attachment_image_src( $custom_logo_id, 'full' );
+
+			$image_src = $logo ? $logo[0] : get_header_image();
+			$image_width = $logo ? $logo[1] : absint(get_custom_header()->width);
+			$image_height = $logo ? $logo[2] : absint(get_custom_header()->height);
+		?>
+
+		<img alt="" src="<?php echo esc_url( $image_src ); ?>" width="<?php echo esc_attr( $image_width ); ?>" height="<?php echo esc_attr( $image_height ); ?>">
+
+		<?php if ( ! $logo ) { ?>
 			<div class="site-branding">
-				<?php Backdrop\Theme\Site\display_site_title(); ?>
-				<?php Backdrop\Theme\Site\display_site_description(); ?>
-			</div>
-		</div>
+			<?php Backdrop\Theme\Site\display_site_title(); ?>
+			<?php Backdrop\Theme\Site\display_site_description(); ?>
+		</div>		
+		<?php } ?>
+	</div>
 	</header>
+
