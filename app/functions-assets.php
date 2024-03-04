3f252930e0b5ded7bc5c9fbbfe731ffdd59cefab
Benjamin Lu <benlumia007@users.noreply.github.com>
2024-03-04 16:02:15 +0000

    updated

--- app/functions-assets.php
+++ app/functions-assets.php
@@ -1,15 +1,16 @@
 <?php
 /**
- * Default assets functions
+ * Default assets related functions and filters.
  *
  * @package   Silver Quantum
  * @author    Benjamin Lu <benlumia007@gmail.com>
- * @copyright 2014-2023. Benjamin Lu
+ * @copyright 2014. Benjamin Lu
  * @license   https://www.gnu.org/licenses/gpl-2.0.html
  * @link      https://luthemes.com/portfolio/silver-quantum
  */
 
 namespace SilverQuantum;
+
 use function Backdrop\Mix\asset;
 
 /**
@@ -25,33 +26,17 @@ add_action( 'wp_enqueue_scripts', function() {
 	wp_enqueue_style( 'silver-quantum-screen', asset( 'assets/css/screen.css' ), null, null );
 
 	// Enqueue theme scripts
-	wp_enqueue_script( 'creativity-app', asset( 'assets/js/app.js' ), [ 'jquery' ], null, true );
+	wp_enqueue_script( 'silver-quantum-app', asset( 'assets/js/app.js' ), [ 'jquery' ], null, true );
 
 	// Enqueue Navigation.
 	wp_enqueue_script( 'silver-quantum-navigation', asset( 'assets/js/navigation.js' ), null, null, true );
 	wp_localize_script( 'silver-quantum-navigation', 'silverQuantumScreenReaderText', [
-		'expand'   => '<span class="screen-reader-text">' . esc_html__( 'expand child menu', 'creativity' ) . '</span>',
-		'collapse' => '<span class="screen-reader-text">' . esc_html__( 'collapse child menu', 'creativity' ) . '</span>',
+		'expand'   => '<span class="screen-reader-text">' . esc_html__( 'expand child menu', 'silver-quantum' ) . '</span>',
+		'collapse' => '<span class="screen-reader-text">' . esc_html__( 'collapse child menu', 'silver-quantum' ) . '</span>',
 	] );
 
 	// Load WordPress' comment-reply script where appropriate.
 	if ( is_singular() && get_option( 'thread_comments' ) && comments_open() ) {
 		wp_enqueue_script( 'comment-reply' );
 	}
-} );
-
-add_action( 'wp_enqueue_scripts', function() {
-	$text_color = get_header_textcolor();
-	if ( get_theme_support( 'custom-header', 'default-text-color' ) === $text_color ) {
-		return;
-	}
-	$value      = display_header_text() ? sprintf( 'color: #%s', esc_html( $text_color ) ) : 'display: none;';
-
-	$custom_css = "
-		.site-header .site-branding .site-title a,
-		.site-header .site-branding .site-description {
-			{$value}
-		}
-	";
-	wp_add_inline_style( 'silver-quantum-screen', $custom_css );
 } );
