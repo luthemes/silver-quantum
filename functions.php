751f2a5161c38fc823bac714ee9d1f0665297227
Benjamin Lu <164923145+techkodeninja@users.noreply.github.com>
2024-09-25 16:08:26 +0000

    updated link

--- functions.php
+++ functions.php
@@ -8,7 +8,7 @@
  * @author    Benjamin Lu <benlumia007@gmail.com>
  * @copyright 2014 Benjamin Lu
  * @license   https://www.gnu.org/licenses/gpl-2.0.html
- * @link      https://github.com/benlumia007/silver-quantum
+ * @link      https://luthemes.com/portfolio/silver-quantum
  */
 
 /** ------------------------------------------------------------------------------------------
