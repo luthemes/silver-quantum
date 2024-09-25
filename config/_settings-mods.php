29b0dd8a6f0b0a2fe8172f15a1a0a7a366c34d34
Benjamin Lu <164923145+techkodeninja@users.noreply.github.com>
2024-09-25 16:28:26 +0000

    mismatch text domain

--- config/_settings-mods.php
+++ config/_settings-mods.php
@@ -42,12 +42,12 @@ return [
 	// Default footer credit text.
 	'theme_footer_custom_credit' => function() {
 		$year = gmdate( 'Y' ); // Get the current year
-		$copyright = sprintf( __( "&#169; %1\$s. %2\$s.", 'white-spektrum' ), $year, Backdrop\Theme\Site\render_home_link() );
+		$copyright = sprintf( __( "&#169; %1\$s. %2\$s.", 'silver-quantum' ), $year, Backdrop\Theme\Site\render_home_link() );
 
 		if ( is_classicpress() ) {
-			$footer_text = sprintf( __( "Powered by %1\$s and %2\$s.", 'white-spektrum' ), Backdrop\Theme\Site\render_cp_link(), Backdrop\Theme\Site\render_theme_link() );
+			$footer_text = sprintf( __( "Powered by %1\$s and %2\$s.", 'silver-quantum' ), Backdrop\Theme\Site\render_cp_link(), Backdrop\Theme\Site\render_theme_link() );
 		} else {
-			$footer_text = sprintf( __( "Powered by %1\$s and %2\$s.", 'white-spektrum' ), Backdrop\Theme\Site\render_wp_link(), Backdrop\Theme\Site\render_theme_link() );
+			$footer_text = sprintf( __( "Powered by %1\$s and %2\$s.", 'silver-quantum' ), Backdrop\Theme\Site\render_wp_link(), Backdrop\Theme\Site\render_theme_link() );
 		}
 
 		return $copyright . ' <br /> ' . $footer_text;
