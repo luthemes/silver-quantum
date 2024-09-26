1a24a5ecac8ad6b457bb7a1cec89cf5e3745c10a
Benjamin Lu <164923145+techkodeninja@users.noreply.github.com>
2024-09-26 17:24:42 +0000

    mismatch text domain, fixed

--- app/Template/ErrorPage.php
+++ app/Template/ErrorPage.php
@@ -112,7 +112,7 @@ class ErrorPage {
 			return;
 		}
 
-		esc_html_e( '404 Not Found', 'amicable' );
+		esc_html_e( '404 Not Found', 'silver-quantum' );
 	}
 
 	/**
@@ -131,7 +131,7 @@ class ErrorPage {
 
 		printf(
 			'<p>%s</p>',
-			esc_html__( 'It looks like you stumbled upon a page that does not exist. Perhaps rolling the dice with a search might help.', 'amicable' )
+			esc_html__( 'It looks like you stumbled upon a page that does not exist. Perhaps rolling the dice with a search might help.', 'silver-quantum' )
 		);
 
 		get_search_form();
