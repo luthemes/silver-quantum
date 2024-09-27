37187dbde1b31f19a3e72bb0c4c06dc8cc979439
Benjamin Lu <164923145+techkodeninja@users.noreply.github.com>
2024-09-27 15:25:36 +0000

    initial commit

---
+++ config/image-sizes.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Image Sizes Config.
+ *
+ * Defines the image sizes that the theme sets.
+ *
+ * @package   Silver Quantum
+ * @author    Benjamin Lu <benlumia007@gmail.com>
+ * @copyright 2024 Benjamin Lu
+ * @license   https://www.gnu.org/licenses/gpl-2.0.html
+ * @link      https://luthemes.com/portfolio/silver-quantum
+ */
+
+ return [
+
+	'silver-quantum-landscape-medium' => [
+		'label'  => __( 'Landscape: Medium', 'silver-quantum' ),
+		'width'  => 640,
+		'height' => 360
+	],
+	'silver-quantum-landscape-large' => [
+		'label'  => __( 'Landscape: Large', 'silver-quantum' ),
+		'width'  => 896,
+		'height' => 504
+	],
+	'silver-quantum-landscape-extra-large' => [
+		'label'  => __( 'Landscape: Extra Large', 'silver-quantum' ),
+		'width'  => 1366,
+		'height' => 768
+	],
+];
